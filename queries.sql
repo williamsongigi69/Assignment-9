18b9e46ca8e9ceff2b32fe198a63a8b713173220
axyperrary <44517869+axyperrary@users.noreply.github.com>
2019-02-26 22:47:48 -0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -117,5 +117,6 @@ OR dp.dept_name LIKE 'Sales';
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 SELECT last_name, COUNT(*) AS frequency
-FROM employeesGROUP BY last_name
+FROM employees
+GROUP BY last_name
 ORDER BY frequency DESC;
